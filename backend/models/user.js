35ef5daf6352cfc9fd47d2d19088394416838f01
Jalal Kalyati <jalal.kalyati@gmail.com>
2024-12-07 15:40:37 -0500

    Create User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,26 @@
+const mongoose = require("mongoose");
 
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
