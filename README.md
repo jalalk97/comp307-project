d78b8bc936eef13904e7a605957ff88b8abd179c
Jalal Kalyati <jalal.kalyati@gmail.com>
2024-12-07 18:26:08 -0500

    Implement user login in the backend

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Make sure you have `node` and `npm` installed.
     cat <<EOF > .env
     PORT=5000
     MONGODB_URI=mongodb+srv://admin:XjLT6K5xj7HVw318@cluster0.ud4jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+    JWT_SECRET=b48636224ec98905f2d932a8196807792ab4a12d114af3e227d830a64d658acd27fea7745d25638d9dc6b0a1fc3270c5d2e9b9894a871777cf957be1e6f2e497
     EOF
     ```
 5. `npm start`
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,45 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const userController = require("./users");
 
 function register(req, res) {
-  userController.createUser(req, res);
+  return userController.createUser(req, res);
 }
 
-function login(req, res) {
+async function login(req, res) {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(401).json({
+      message: "Email or password is invalid",
+    });
+  }
+
+  const user = await User.findOne({ email }).lean();
+  const isPasswordCorrect = user && await bcrypt.compare(password, user.password);
+
+  if (!user || !isPasswordCorrect) {
+    return res.status(401).json({
+      message: "Email or password is invalid",
+    });
+  }
+
+  const userForToken = {
+    email: user.email,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.JWT_SECRET);
+
+  res.status(200).json({
+    token,
+    user: {
+      email: user.email,
+      id: user._id,
+    },
+  });
 }
 
 function logout(req, res) {
