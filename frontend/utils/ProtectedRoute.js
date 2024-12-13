bcf17b368cfe0c6198fe627ac2c81ae93e32ed5f
patwilson3 <patrick.wilson2@mail.mcgill.ca>
2024-12-13 17:04:41 -0500

    adding protected route

--- frontend/utils/ProtectedRoute.js
+++ frontend/utils/ProtectedRoute.js
@@ -4,7 +4,7 @@ import React from 'react';
 //tobe completed
 const ProtectedRoute = ({children}) => {
     const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/login" />;
+    return token ? children : <Navigate to="/Login" />; //incase user is already logged out, if token does not exist redirects to login page
 }
 
 export default ProtectedRoute;
