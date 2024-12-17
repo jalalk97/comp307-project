0b3eb3ed57c77a986ca18b9b521059d9dabdbfa8
Jalal Kalyati <jalal.kalyati@gmail.com>
2024-12-17 00:26:21 -0500

    Fix deployment by updating baseUrl in apiSlice

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   // TODO change baseUrl before deploying the app
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000",
+    baseUrl: "/",
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth;
       if (token) {
