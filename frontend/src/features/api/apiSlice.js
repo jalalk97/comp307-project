5dba5db3c468880fe10c7ee2656624eb80f4dd0e
Jalal Kalyati <jalal.kalyati@gmail.com>
2024-12-17 02:20:18 -0500

    Fix deployment

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   // TODO change baseUrl before deploying the app
   baseQuery: fetchBaseQuery({
-    baseUrl: "/",
+    // baseUrl: "http://localhost:4000", // uncomment for local development
+    baseUrl: "/", // uncomment for deployment
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth;
       if (token) {
--- frontend/utils/ProtectedRoute.jsx
+++ frontend/utils/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { selectToken } from "../src/features/auth/authSlice";
 const ProtectedRoute = ({ children }) => {
   const token = useSelector(selectToken);
 
-  return token ? children : <Navigate to="/Login" />; //incase user is already logged out, if token does not exist redirects to login page
+  return token ? children : <Navigate to="/login" />; //incase user is already logged out, if token does not exist redirects to login page
 };
 
 export default ProtectedRoute;
