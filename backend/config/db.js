3928bf87a9c218a42326d38f73f916eb8fc9e02c
Jalal Kalyati <jalal.kalyati@gmail.com>
2024-12-20 18:10:09 -0500

    Delete secrets

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI ||
-        "mongodb+srv://admin:XjLT6K5xj7HVw318@cluster0.ud4jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (e) {
     console.log(e);
   }
